1c806967ed923b75bf9f536475f59ec609abde4d
xli2522 <50414011+xli2522@users.noreply.github.com>
2021-01-04 17:33:49 -0500

    updated new function and demo

--- demo.py
+++ demo.py
@@ -3,7 +3,34 @@
                 ###      Time-lapse Tool Demo     ###
                 ###  *Image to video with openCV  ###
 
-###### Use Time-lapse Tool.py ######
+####################################
+###### Use time_lapse_tool.py ######
+####################################
 from time_lapse_tool import *
 
 timelapse('demo.avi', 10, r"C:\Users\xli2522\Documents\GitHub\test_image")      # change folder address to your own
+
+#################################
+###### Use plt_to_video.py ######
+#################################
+import matplotlib.pyplot as plt
+from plt_to_video import *
+
+# waht your matplotlib loop might look like
+def test_matplot_loop(n):
+    '''This function tests the function
+        Input:      n   number of frames
+    '''
+    images = []     # empty image container
+    plt.figure()    # initialize matplotlib figure
+    for i in range(n):
+        plt.plot([np.random.randint(2), np.random.randint(2)])      # generate random plots for demo
+        plt.title("test" + str(i))
+        images = savebuff(plt, images)      # save image in in-memory location
+        plt.clf()
+    
+    construct(images, 'matplot_demo.avi', 5)        # construct video; 5 fps
+
+    return 
+
+test_matplot_loop(100)      # construct a demo video with 100 frames
---
+++ plt_to_video.py
@@ -0,0 +1,58 @@
+                ###        © Xiyuan Li 2021       ###
+                ###        xli2522 @ github       ###
+                ###    Matplotlib plt to video    ###
+                ###  *Image to video with openCV  ###
+import cv2
+import numpy as np
+import os.path
+import glob
+import io
+from PIL import Image
+
+def savebuff(frame, container):
+    '''Function saves image in in-memory location
+        Inputs:     frame   (matplotlib image)  
+                    container   (list)     empty image container
+        Output:     container   (list)      image container with added frame location
+    '''
+
+    buf = io.BytesIO()      # create in-memory buffer location
+    frame.savefig(buf, format='jpeg')       # save buffer image in jpeg format (*3 channels)
+    container.append(buf)       # add the 
+
+    return container
+
+def construct(container, title, fps, inspect=True):
+    '''Function constructs video from images in the container.
+        Inputs:     container   (list)      image container with frame location
+                    title   (string)     video title + .avi
+                    fps     (double)     time-lapse video frames per second 
+                    inspect    (boolean)       True (default)/False
+        Output:
+                    video
+    '''
+    #* Image Processing
+    cvImages = []     # image container for opencv
+    for image in container:       # read all sorted images 
+        cvIm = np.asarray(Image.open(image))
+        if cvIm is not None:    
+            cvImages.append(cvIm)       # add images to opencv image container
+
+    frame_spec = cvImages[0]        # sample the first image in the container
+    height, width, layers = frame_spec.shape        # get image specs
+
+    #* Video Construction
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')        # set video specs
+    video = cv2.VideoWriter(title, fourcc, fps, (width, height))  # create opencv video object
+
+    for frame in cvImages:          # access each frame in the image container
+        video.write(frame)         # write frame
+        if inspect:         # if True, show frame
+            cv2.imshow('Inspection', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):       # stop the process when interupted by key q
+            break 
+
+    cv2.destroyAllWindows()     # destroy any open window
+    video.release()            # release the video
+
+    return 
